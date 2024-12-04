f2eb489a42e6c5c566ecd194d715275a35b76b0d
Justin Jemison <jjemi8884@gmail.com>
2024-12-04 08:33:04 -0800

    got test to 61 percent (i really dont think its accurate)

--- src/__test__/LandingPage.test.js
+++ src/__test__/LandingPage.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { render, screen, toBeInTheDocument } from '@testing-library/react';
+import { render, screen, cleanup, getByText, queryByText, cleanup } from '@testing-library/react';
 import LandingPage from '../components/LandingPage';
+import { MemoryRouter } from 'react-router-dom';
+
 
 test('renders LandingPage form', () => {
-  render(<LandingPage />);
+  render(
+  <MemoryRouter initialEntries={['/']}>
+    <LandingPage />
+  </MemoryRouter>);
   
+  // Check if the form elements are rendered
+  expect(getByText(/Welcome to the Pet Tracker/i)).toBeInTheDocument();
+  // expect(screen.getByRole(<h1>Welcome to the Pet Tracker</h1>)).toBeInTheDocument();
+  // expect(getByText('Please Sign into the App.')).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Log In With GitHub' }));
 });
